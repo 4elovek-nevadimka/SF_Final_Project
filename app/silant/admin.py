0bd18bbd251c087ad27a250f477e8ff1c6fc33c3
nevadimka <myregbox@outlook.com>
2022-12-17 19:29:49 +0300

    added models to admin panel

--- app/silant/admin.py
+++ app/silant/admin.py
@@ -1,3 +1,19 @@
 from django.contrib import admin
 
-# Register your models here.
+from silant.models import *
+
+admin.site.register(User)
+
+admin.site.register(Machine)
+admin.site.register(Maintenance)
+admin.site.register(Claim)
+
+admin.site.register(VehicleModel)
+admin.site.register(EngineModel)
+admin.site.register(TransmissionModel)
+admin.site.register(DriveAxleModel)
+admin.site.register(SteeringBridgeModel)
+
+admin.site.register(MaintenanceType)
+admin.site.register(FailureNode)
+admin.site.register(RecoveryMethod)
--- app/silant/models.py
+++ app/silant/models.py
@@ -14,6 +14,10 @@ class User(AbstractUser):
     description = models.CharField(max_length=512, null=True, blank=True, verbose_name='Описание')
     role = models.CharField(max_length=2, choices=CHOICES, default='CL', verbose_name='Роль')
 
+    class Meta:
+        verbose_name = 'Пользователь'
+        verbose_name_plural = 'Пользователи'
+
     def __str__(self):
         return f'{self.name}'
 
