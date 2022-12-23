9f81bf35bf9d771ca7c734a1a9c38dbc3c2e3d68
nevadimka <myregbox@outlook.com>
2022-12-24 00:49:31 +0300

    added custom admin models

--- app/silant/admin.py
+++ app/silant/admin.py
@@ -2,11 +2,36 @@ from django.contrib import admin
 
 from silant.models import *
 
-admin.site.register(User)
 
-admin.site.register(Machine)
-admin.site.register(Maintenance)
-admin.site.register(Claim)
+@admin.register(User)
+class User(admin.ModelAdmin):
+    fields = ('username', 'email', 'password', 'role', 'name', 'description',)
+    list_display = ('username', 'role', 'name', 'description',)
+    list_filter = ('role', 'name', 'description',)
+    search_fields = ('name', 'description',)
+
+
+@admin.register(Machine)
+class Machine(admin.ModelAdmin):
+    list_display = ('serial_number', 'vehicle_model', 'engine_model', 'transmission_model', 'drive_axle_model',
+                    'steering_bridge_model', 'shipment_date', 'equipment', 'client', 'service_company',)
+    list_filter = ('vehicle_model', 'engine_model', 'transmission_model', 'drive_axle_model', 'steering_bridge_model',)
+    search_fields = ('serial_number',)
+
+
+@admin.register(Maintenance)
+class Maintenance(admin.ModelAdmin):
+    list_display = ('machine', 'maintenance_type', 'maintenance_date', 'operating_time', 'work_order_number',
+                    'service_company',)
+    list_filter = ('maintenance_type', 'maintenance_date',)
+
+
+@admin.register(Claim)
+class Claim(admin.ModelAdmin):
+    list_display = ('machine', 'failure_date', 'operating_time', 'failure_node', 'recovery_method', 'recovery_date',
+                    'machine_downtime',)
+    list_filter = ('failure_date', 'failure_node', 'recovery_method',)
+
 
 admin.site.register(VehicleModel)
 admin.site.register(EngineModel)
