25a6db257fc5ddf736944ec0bf5cb9b4e89545b2
nevadimka <myregbox@outlook.com>
2023-01-05 12:53:08 +0300

    added guest mode (for non_authenticated users), added filters

--- app/silant/views.py
+++ app/silant/views.py
@@ -1,6 +1,6 @@
-from django.shortcuts import render
 from django.views.generic import ListView
 
+from silant.filters import MachineFilter
 from silant.models import Machine
 
 
@@ -8,3 +8,11 @@ class Index(ListView):
     model = Machine
     template_name = 'index.html'
     context_object_name = 'machine'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        if self.request.user.is_authenticated:
+            pass
+        else:
+            context['machine_filter'] = MachineFilter(self.request.GET, queryset=self.get_queryset())
+        return context
