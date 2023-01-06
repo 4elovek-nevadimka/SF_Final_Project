4fd30b0ad61b2823fe87e4b0be1cd14cca386bd1
nevadimka <myregbox@outlook.com>
2023-01-06 12:37:32 +0300

    added filters, updated index view, added images

--- app/silant/filters.py
+++ app/silant/filters.py
@@ -1,9 +1,35 @@
 from django_filters import FilterSet
 
-from .models import Machine
+from .models import Machine, Maintenance, Claim
+
+
+class GuestFilter(FilterSet):
+    class Meta:
+        model = Machine
+        fields = ('serial_number',)
 
 
 class MachineFilter(FilterSet):
     class Meta:
         model = Machine
-        fields = ('serial_number',)
+        fields = ('vehicle_model',
+                  'engine_model',
+                  'transmission_model',
+                  'drive_axle_model',
+                  'steering_bridge_model',)
+
+
+class MaintenanceFilter(FilterSet):
+    class Meta:
+        model = Maintenance
+        fields = ('maintenance_type',
+                  'machine__serial_number',
+                  'service_company',)
+
+
+class ClaimFilter(FilterSet):
+    class Meta:
+        model = Claim
+        fields = ('failure_node',
+                  'recovery_method',
+                  'machine__service_company',)
--- app/silant/views.py
+++ app/silant/views.py
@@ -1,7 +1,7 @@
 from django.views.generic import ListView
 
-from silant.filters import MachineFilter
-from silant.models import Machine
+from silant.filters import GuestFilter, MachineFilter, MaintenanceFilter, ClaimFilter
+from silant.models import Machine, Maintenance, Claim
 
 
 class Index(ListView):
@@ -12,7 +12,13 @@ class Index(ListView):
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
         if self.request.user.is_authenticated:
-            pass
+            context['machine_filter'] = MachineFilter(
+                self.request.GET, queryset=Machine.objects.all())
+            context['maintenance_filter'] = MaintenanceFilter(
+                self.request.GET, queryset=Maintenance.objects.all())
+            context['claim_filter'] = ClaimFilter(
+                self.request.GET, queryset=Claim.objects.all())
+            return context
         else:
-            context['machine_filter'] = MachineFilter(self.request.GET, queryset=self.get_queryset())
+            context['guest_filter'] = GuestFilter(self.request.GET, queryset=self.get_queryset())
         return context
